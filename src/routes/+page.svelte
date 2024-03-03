f0035143d09d23e3caa6b8c6ccc633f8922628a5
Jimmy McBride <jimmy@ravebizz.com>
2024-03-02 18:25:21 -0600

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="my-5">
+		<h1 class="h1">Makers Forge</h1>
+	</div>
+</div>
