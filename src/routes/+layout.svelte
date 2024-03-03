d5e5a92b5387f39adf9590c70e932a5628bdf23e
Jimmy McBride <jimmy@ravebizz.com>
2024-03-03 11:36:21 -0600

    added some updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.postcss';
+	import '../app.pcss';
 
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
@@ -9,6 +9,17 @@
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
 	import typescript from 'highlight.js/lib/languages/typescript';
+    import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+    import { afterNavigate } from '$app/navigation';
+    import {GithubSolid, LifeSaverSolid, MailBoxSolid} from 'flowbite-svelte-icons';
+
+    afterNavigate((params) => {
+        const isNewPage = params.from?.url.pathname !== params.to?.url.pathname;
+        const elemPage = document.querySelector('#page');
+        if (isNewPage && elemPage !== null) {
+            elemPage.scrollTop = 0;
+        }
+    });
 
 	hljs.registerLanguage('xml', xml); // for HTML
 	hljs.registerLanguage('css', css);
@@ -22,4 +33,26 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<slot />
+<AppShell class="max-w-[100vw] min-h-[100vh]">
+    <svelte:fragment slot="header">
+        <AppBar>
+            <svelte:fragment slot="lead">
+                <div class="row space-x-2">
+                    <LifeSaverSolid />
+                    <div>Makers Forge</div>
+                </div>
+            </svelte:fragment>
+
+            <svelte:fragment slot="trail">
+                <GithubSolid />
+                <MailBoxSolid />
+            </svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
+    <slot />
+    <svelte:fragment slot="pageFooter">
+        <div class="m-4">
+            Page Footer
+        </div>
+    </svelte:fragment>
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto drape">
 	<div class="my-5">
 		<h1 class="h1">Makers Forge</h1>
 	</div>
