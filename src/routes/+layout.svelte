bb3526f00c1760744384a8b09811742222bd48a5
Jimmy McBride <46766874+JimmyMcBride@users.noreply.github.com>
2024-03-04 19:36:09 -0500

    Makers Forge Logo (#1)
    * added metadata
    
    * updated formatting and linting settings
    
    * added makers forge logo
    
    * updated icons to work when you click them

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,58 +1,66 @@
 <script>
-	import '../app.pcss';
+	import "../app.pcss"
 
 	// Highlight JS
-	import hljs from 'highlight.js/lib/core';
-	import 'highlight.js/styles/github-dark.css';
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
-	import xml from 'highlight.js/lib/languages/xml'; // for HTML
-	import css from 'highlight.js/lib/languages/css';
-	import javascript from 'highlight.js/lib/languages/javascript';
-	import typescript from 'highlight.js/lib/languages/typescript';
-    import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-    import { afterNavigate } from '$app/navigation';
-    import {GithubSolid, LifeSaverSolid, MailBoxSolid} from 'flowbite-svelte-icons';
+	import hljs from "highlight.js/lib/core"
+	import "highlight.js/styles/github-dark.css"
+	import { storeHighlightJs } from "@skeletonlabs/skeleton"
+	import xml from "highlight.js/lib/languages/xml" // for HTML
+	import css from "highlight.js/lib/languages/css"
+	import javascript from "highlight.js/lib/languages/javascript"
+	import typescript from "highlight.js/lib/languages/typescript"
+	import { AppShell, AppBar } from "@skeletonlabs/skeleton"
+	import { afterNavigate } from "$app/navigation"
+	import { GithubSolid, MailBoxSolid } from "flowbite-svelte-icons"
 
-    afterNavigate((params) => {
-        const isNewPage = params.from?.url.pathname !== params.to?.url.pathname;
-        const elemPage = document.querySelector('#page');
-        if (isNewPage && elemPage !== null) {
-            elemPage.scrollTop = 0;
-        }
-    });
+	afterNavigate((params) => {
+		const isNewPage = params.from?.url.pathname !== params.to?.url.pathname
+		const elemPage = document.querySelector("#page")
+		if (isNewPage && elemPage !== null) {
+			elemPage.scrollTop = 0
+		}
+	})
 
-	hljs.registerLanguage('xml', xml); // for HTML
-	hljs.registerLanguage('css', css);
-	hljs.registerLanguage('javascript', javascript);
-	hljs.registerLanguage('typescript', typescript);
-	storeHighlightJs.set(hljs);
+	hljs.registerLanguage("xml", xml) // for HTML
+	hljs.registerLanguage("css", css)
+	hljs.registerLanguage("javascript", javascript)
+	hljs.registerLanguage("typescript", typescript)
+	storeHighlightJs.set(hljs)
 
 	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom"
+	import { storePopup } from "@skeletonlabs/skeleton"
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow })
 </script>
 
 <AppShell class="max-w-[100vw] min-h-[100vh]">
-    <svelte:fragment slot="header">
-        <AppBar>
-            <svelte:fragment slot="lead">
-                <div class="row space-x-2">
-                    <LifeSaverSolid />
-                    <div>Makers Forge</div>
-                </div>
-            </svelte:fragment>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<div class="row space-x-2">
+					<img class="w-6" src="/makers_forge_logo.png" alt="makers forge logo" />
+					<div>Makers Forge</div>
+				</div>
+			</svelte:fragment>
 
-            <svelte:fragment slot="trail">
-                <GithubSolid />
-                <MailBoxSolid />
-            </svelte:fragment>
-        </AppBar>
-    </svelte:fragment>
-    <slot />
-    <svelte:fragment slot="pageFooter">
-        <div class="m-4">
-            Page Footer
-        </div>
-    </svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					href="https://github.com/JimmyMcBride/makers-forge"
+					class="badge-icon variant-filled"
+					target="_blank"
+				>
+					<GithubSolid />
+				</a>
+				<a href="mailto:hello@makersforge.io" class="badge-icon variant-filled">
+					<MailBoxSolid />
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="pageFooter">
+		<div class="m-4 row justify-center">
+			<p>&copy; 2024 Makers Forge. All rights reserved.</p>
+		</div>
+	</svelte:fragment>
 </AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,35 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	import { dev } from "$app/environment"
+
+	const title = "Makers Forge"
+	const description = "Helping makers grow 🌱"
+	const url = dev ? "http://localhost:5173" : "https://makersforge.io"
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+
+	<meta name="description" content={description} />
+
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content={`${url}/blog`} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:site_name" content={title} />
+	<meta property="og:image" content="/makers_forge_logo.png" />
+
+	<!--	TODO: Replace these with Makers Forge twitter handle-->
+	<!--	<meta name="twitter:site" content="@McBride1105" />-->
+	<!--	<meta name="twitter:creator" content="@McBride1105" />-->
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:image:src" content="/makers_forge_logo.png" />
+	<meta name="twitter:widgets:new-embed-design" content="on" />
+
+	<meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
+	<meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
+</svelte:head>
 
 <div class="container h-full mx-auto drape">
 	<div class="my-5">
